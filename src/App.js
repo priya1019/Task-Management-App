3ce410937f68cdfcc8dc08917eb0effbd9e0461f
priya.pandey3 <priya.pandey@mymedisage.com>
2024-01-04 07:57:42 +0530

    React App Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo.svg";
+function App() {
+  return (
+    <div className="App">
+      <header className="flex-items">
+        <img src={logo} height={40} width={40} alt="Task Management App" />
+        <h1>Task Management App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
